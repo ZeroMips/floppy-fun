b6fd1d326d80b6674bd64325cd27d06888bf7987
Dirk Eibach <dirk.eibach@gmx.de>
2024-01-03 21:30:34 +0100

    Use python construct module for matching sector header

--- mfm.py
+++ mfm.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from construct import *
 
 class MFMSym(Enum):
 	S = 0
@@ -12,6 +13,19 @@ class SyncState(Enum):
 	WAIT_L1 = 2
 	WAIT_M1 = 3
 	DONE = 4
+
+class ClockState(Enum):
+	NO_SYNC = 0
+	DATA_0 = 1
+	DATA_1 = 2
+	CLOCK_0 = 3
+	CLOCK_1 = 4
+
+class HeaderState(Enum):
+	WAIT_A1 = 0
+	WAIT_FX = 1
+	WAIT_DATA = 2
+	WAIT_CRC = 3
 
 def classify(dt):
 	if dt < 1.5:
@@ -52,13 +66,27 @@ def SyncMachine(sync_state, sym):
 
 	return sync_state
 
-def Stream(stream, sym):
+def Stream(stream, sym, clock_state):
+	# these clock states can never be correct, because all symbols start with 1
+	if clock_state == ClockState.DATA_1:
+		clock_state = ClockState.NO_SYNC
+	elif clock_state == ClockState.CLOCK_1:
+		clock_state = ClockState.NO_SYNC
+
 	if sym == MFMSym.S:
 		stream.extend([1, 0])
+		# clock state does not get modified
 	elif sym == MFMSym.M:
 		stream.extend([1, 0, 0])
+		if clock_state == ClockState.DATA_0:
+			clock_state = ClockState.CLOCK_0
+		elif clock_state == ClockState.CLOCK_0:
+			clock_state = ClockState.DATA_0
 	elif sym == MFMSym.L:
 		stream.extend([1, 0, 0, 0])
+		# clock state does not get modified
+
+	return clock_state
 
 def Assemble(stream, read_idx, out):
 	char = 0
@@ -76,8 +104,11 @@ with open("WFM01.BIN", "rb") as f:
 	stream = []
 	symbols = []
 	out = []
+	blocks = []
+	sectors = []
 	read_idx = 0
 	sync_done = 0
+	clock_state = ClockState.NO_SYNC
 
 	while (byte := f.read(1)):
 		# read data falling edge
@@ -88,24 +119,56 @@ with open("WFM01.BIN", "rb") as f:
 			sym = classify(dt)
 			symbols.append(sym)
 			sync_state = SyncMachine(last_sync_state, sym)
-			Stream(stream, sym)
+			clock_state = Stream(stream, sym, clock_state)
 			if (last_sync_state != sync_state) and (sync_state == SyncState.DONE):
 				print('Sync done')
 				read_idx = len(stream) - 14
 				sync_done = 1
+				clock_state = ClockState.CLOCK_0
+				if len(out) > 3:
+					blocks.append(out)
+					out = []
 			if sync_done:
 				last_read_idx = read_idx
 				read_idx = Assemble(stream, read_idx, out)
 			ctr = 0
-			print(dt, byte, sym, stream[last_stream:len(stream)], [(hex(no),ascii(chr(no))) for no in out[last_out:len(out)]])
+			print(dt, byte, sym, clock_state, stream[last_stream:len(stream)], [(hex(no),ascii(chr(no))) for no in out[last_out:len(out)]])
 		else:
 			ctr = ctr + 1
 		last_data = byte[0] & 0x02
 		last_sync_state = sync_state
 
+	if len(out) > 3:
+		blocks.append(out)
+		out = []
+
+	for block in blocks:
+		try:
+			d = Select(
+				Struct(
+					"Signature" / Const(b'\xa1\xa1\xa1'),
+					"Type" / Const(b'\xfe'),
+					"TrackNumber" / Byte,
+					"Head" / Byte,
+					"SectorNumber" / Byte,
+					"SectorSize" / Byte,
+					"CRC" / Int16ub
+				),
+				Struct(
+					"Signature" / Const(b'\xa1\xa1\xa1'),
+					"Type" / Select(Const(b'\xfa'), Const(b'\xfb')),
+					"Data" / Array(512,Byte),
+					"CRC" / Int16ub
+				)
+			)
+			sectors.append(d.parse(bytes(block)))
+		except (ConstError, SelectError):
+			print("Could not parse")
+
 #	print(symbols)
 #	print(stream)
-	print([(hex(no),ascii(chr(no))) for no in out])
+	print(sectors)
+#	print([(hex(no),ascii(chr(no))) for no in out])
 
 	with open('out.bin', 'wb') as wf:
 		wf.write(bytes(out))
